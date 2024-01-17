ffd4be61f7e8c424bd6543c0b1507d6dcd16290a
devxavierr <devxavierr@gmail.com>
2024-01-16 23:30:13 -0300

    init

---
+++ src/main/java/com/xavier/catalogmarketplace/CatalogMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.catalogmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xavier/catalogmarketplace/CatalogMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.catalogmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
