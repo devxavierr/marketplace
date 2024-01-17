ba64bdfd97aa9e641c17cdcf68d1d55b795c8bc9
devxavierr <devxavierr@gmail.com>
2024-01-16 23:54:32 -0300

    config mongoDB

--- src/main/java/com/xavier/catalogmarketplace/CatalogMarketplaceApplication.java
+++ src/main/java/com/xavier/catalogmarketplace/CatalogMarketplaceApplication.java
@@ -2,7 +2,10 @@ package com.xavier.catalogmarketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+
+@EnableMongoRepositories
 @SpringBootApplication
 public class CatalogMarketplaceApplication {
 
